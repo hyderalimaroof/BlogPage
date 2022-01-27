f72dbfec18d875470b43bda2dd49778e3bd84ea0
hyderalimaroof <hyderalimaroof@gmail.com>
2022-01-27 12:24:08 +0500

    set URL

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('users/', include('users.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
